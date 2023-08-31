5379e537ab514795bb59385807dc322aa078fe82
rdraward <riley@gadget.dev>
2023-08-31 10:03:25 -0400

    get mocking working properly

--- test/frontend/routes/signed-in.test.jsx
+++ test/frontend/routes/signed-in.test.jsx
@@ -2,12 +2,27 @@ import { beforeAll, describe, expect, it, vi } from "vitest";
 import renderer from "react-test-renderer";
 import SignedIn from "../../../frontend/routes/signed-in";
 
-const defaultUser = {
-  id: 0,
-  email: "test@gmail.com",
-  firstName: "Carl",
-  lastName: "Weathers",
-};
+// Gadget React hooks, such as useUser and useFindMany, can be mocked to return data
+const mocks = vi.hoisted(() => {
+  const defaultUser = {
+    id: 0,
+    email: "test@gmail.com",
+    firstName: "Carl",
+    lastName: "Weathers",
+  };
+
+  return {
+    useUser: () => defaultUser,
+    useFindMany: vi.fn(),
+  };
+});
+
+vi.mock("@gadgetinc/react", () => {
+  return {
+    useUser: mocks.useUser,
+    useFindMany: mocks.useFindMany,
+  };
+});
 
 // this is an example mocking out the Gadget API client and useUser hook
 describe("snapshot test for frontend/routes/signed-in.jsx", () => {
@@ -16,8 +31,6 @@ describe("snapshot test for frontend/routes/signed-in.jsx", () => {
     vi.mock("../../../frontend/api", () => {
       return {
         api: {
-          // alternative option: replace this mock with the test api and run full-stack integration tests
-          // you could wipe the db using api.post.deleteMany() to reset the db, then create one or more post records before capturing snapshots
           user: {},
           post: {},
         },
@@ -26,51 +39,34 @@ describe("snapshot test for frontend/routes/signed-in.jsx", () => {
   });
 
   it("should render the default page for signed-in users", () => {
-    // Gadget React hooks, such as useUser and useFindMany, can be mocked to return data
-    vi.mock("@gadgetinc/react", () => {
-      return {
-        useUser: () => defaultUser,
-        useFindMany: () => {
-          return [
-            {
-              data: [
-                {
-                  id: 1,
-                  title: "Test post"
-                },
-              ],
-              fetching: false,
-              error: null,
-            },
-          ];
-        },
-      };
-    });
+    // Provide mock data for the useFindMany
+    mocks.useFindMany.mockReturnValue([
+      {
+        data: [
+          {
+            id: 1,
+            title: "Test post",
+          },
+        ],
+        fetching: false,
+        error: null,
+      },
+    ]);
 
     const tree = renderer.create(<SignedIn />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("should render the loading message for signed-in users", () => {
-    // Gadget React hooks, such as useUser and useFindMany, can be mocked to return data
-    vi.mock("@gadgetinc/react", () => {
-      return {
-        useUser: () => defaultUser,
-        // returned values for useFindMany hooks can be mocked in individual tests to produce different snapshots
-        useFindMany: () => {
-          return [
-            {
-              data: null,
-              fetching: true,
-              error: null,
-            },
-          ];
-        },
-      };
-    });
+    mocks.useFindMany.mockReturnValue([
+      {
+        data: null,
+        fetching: true,
+        error: null,
+      },
+    ]);
 
     const tree = renderer.create(<SignedIn />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
-
